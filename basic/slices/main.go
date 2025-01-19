562221fc3f6d5abee3e0be8b92597f2924146d35
Tusharjagi <tusharjagi@gmail.com>
2025-01-19 16:03:45 +0530

    slices

---
+++ basic/slices/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to slices")
+
+	var fruitList = []string{"apple", "tomato", "peach"}
+	fmt.Printf("Type of fruit list is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[:3])
+	fmt.Println(fruitList)
+
+	highScore := make([]int, 4)
+
+	highScore[0] = 234
+	highScore[1] = 945
+	highScore[2] = 465
+	highScore[3] = 867
+	// highScore[4] = 777
+
+	highScore = append(highScore, 555, 666, 321)
+
+	fmt.Println(highScore)
+
+	sort.Ints(highScore)
+	
+	fmt.Println(highScore)
+	
+	fmt.Println(sort.IntsAreSorted(highScore))
+}
